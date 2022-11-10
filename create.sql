8a6c3809bb02e970449ecb8f3735285066e33a5e
Vsevolod01 <seva10052001@gmail.com>
2022-11-11 01:55:00 +0300

    Aš jau miręs, bet baigiau tuos suvaržymus

--- create.sql
+++ create.sql
@@ -16,38 +16,46 @@ DROP TABLE IF EXISTS addresses;
 CREATE TABLE recommendations
 (
     id   SERIAL PRIMARY KEY,
-    name VARCHAR(64) NOT NULL
+    name VARCHAR(64) NOT NULL,
+    CONSTRAINT uniq_rec UNIQUE (name)
 );
 CREATE TABLE severity
 (
     id         SERIAL PRIMARY KEY,
-    treat_type VARCHAR(64)
+    treat_type VARCHAR(64),
+    CONSTRAINT uniq_sev UNIQUE (treat_type)
 );
 CREATE TABLE specialities
 (
     id          SERIAL PRIMARY KEY,
     name        VARCHAR(64)  NOT NULL,
-    description VARCHAR(255) NOT NULL
+    description VARCHAR(255) NOT NULL,
+    CONSTRAINT uniq_spec1 UNIQUE (name),
+    CONSTRAINT uniq_spec2 UNIQUE (description)
 );
 CREATE TABLE symptoms
 (
     id          SERIAL PRIMARY KEY,
     severity_id INTEGER REFERENCES severity NOT NULL,
     name        VARCHAR(64)                 NOT NULL,
-    description VARCHAR(255)                NOT NULL
+    description VARCHAR(255)                NOT NULL,
+    CONSTRAINT uniq_sympt1 UNIQUE (name),
+    CONSTRAINT uniq_sympt2 UNIQUE (description)
 );
 CREATE TABLE rec_sym
 (
     id                 SERIAL PRIMARY KEY,
     recommendations_id INTEGER REFERENCES recommendations NOT NULL,
-    symptoms_id        INTEGER REFERENCES symptoms        NOT NULL
+    symptoms_id        INTEGER REFERENCES symptoms        NOT NULL,
+    CONSTRAINT uniq_rs UNIQUE (recommendations_id, symptoms_id)
 );
 CREATE TABLE points
 (
     id              SERIAL PRIMARY KEY,
     symptoms_id     INTEGER REFERENCES symptoms     NOT NULL,
     specialities_id INTEGER REFERENCES specialities NOT NULL,
-    points          INTEGER                         NOT NULL
+    points          INTEGER                         NOT NULL,
+    CONSTRAINT uniq_pnts UNIQUE (symptoms_id, specialities_id)
 );
 CREATE TABLE doctors
 (
@@ -59,25 +67,30 @@ CREATE TABLE doctors
 CREATE TABLE status
 (
     id     SERIAL PRIMARY KEY,
-    status VARCHAR(64) NOT NULL
+    status VARCHAR(64) NOT NULL,
+    CONSTRAINT uniq_st UNIQUE (status)
 );
 CREATE TABLE addresses
 (
     id       SERIAL PRIMARY KEY,
     address  VARCHAR(64) NOT NULL,
-    district VARCHAR(64) NOT NULL
+    district VARCHAR(64) NOT NULL,
+    CONSTRAINT uniq_addr UNIQUE (address)
 );
 CREATE TABLE clinics
 (
     id           SERIAL PRIMARY KEY,
     addresses_id INTEGER REFERENCES addresses NOT NULL,
-    number       INTEGER                      NOT NULL
+    number       INTEGER                      NOT NULL,
+    CONSTRAINT uniq_clnc1 UNIQUE (addresses_id),
+    CONSTRAINT uniq_clnc2 UNIQUE (number)
 );
 CREATE TABLE work
 (
     id         SERIAL PRIMARY KEY,
     doctors_id INTEGER REFERENCES doctors NOT NULL,
-    clinics_id INTEGER REFERENCES clinics NOT NULL
+    clinics_id INTEGER REFERENCES clinics NOT NULL,
+    CONSTRAINT uniq_wrk UNIQUE (doctors_id, clinics_id)
 );
 CREATE TABLE patients
 (
@@ -92,17 +105,18 @@ CREATE TABLE _users
     id          SERIAL PRIMARY KEY,
     patients_id INTEGER REFERENCES patients NOT NULL,
     login       VARCHAR(64)                 NOT NULL,
-    password    VARCHAR(64)                 NOT NULL
+    password    VARCHAR(64)                 NOT NULL,
+    CONSTRAINT uniq_usr UNIQUE (patients_id)
 );
 CREATE TABLE appointments
 (
     id          SERIAL PRIMARY KEY,
-    clinics_id  INTEGER REFERENCES clinics NOT NULL,
-    doctors_id  INTEGER REFERENCES doctors NOT NULL,
+    work_id  INTEGER REFERENCES work NOT NULL,
     patients_id INTEGER REFERENCES patients,
     status_id   INTEGER REFERENCES status  NOT NULL,
     date        TIMESTAMP                  NOT NULL,
     at_home     BOOLEAN,
+    CONSTRAINT uniq_app1 UNIQUE (date),
     CONSTRAINT free CHECK (status_id = 1 and patients_id IS NULL and at_home IS NULL or
                            status_id = 2 and patients_id IS NOT NULL and at_home IS NOT NULL)
 );
