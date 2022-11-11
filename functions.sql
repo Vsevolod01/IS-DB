43ba2649561d1c15279395c92b15b1face102528
LFiosx <kurnosovaiv@mail.ru>
2022-11-11 11:33:53 +0300

    update

--- functions.sql
+++ functions.sql
@@ -1,6 +1,6 @@
-create function update_adr() returns trigger
-    language plpgsql
-as
+CREATE FUNCTION update_adr() RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS
 $$
 BEGIN
     UPDATE addresses SET cnt=addresses.cnt - 1 WHERE addresses.id = OLD.addresses_id;
@@ -12,9 +12,9 @@ BEGIN
 END;
 $$;
 
-create function add_adr(patient_id integer, adr character varying, distr character varying) returns void
-    language plpgsql
-as
+CREATE FUNCTION add_adr(patient_id INTEGER, adr CHARACTER VARYING, distr CHARACTER VARYING) RETURNS void
+    LANGUAGE plpgsql
+AS
 $$
 DECLARE
     new_id INTEGER;
@@ -22,7 +22,7 @@ BEGIN
     IF adr NOT IN (SELECT address FROM addresses) THEN
         INSERT INTO addresses (address, district)
         VALUES (adr, distr);
-    end if;
+    END IF;
     new_id := (SELECT addresses.id FROM addresses WHERE address = adr);
     UPDATE patients SET addresses_id = new_id WHERE patients.id = patient_id;
 END;
