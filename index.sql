d498ac642cde812f8aabdf56203db8ca829414af
Vsevolod01 <seva10052001@gmail.com>
2022-11-11 03:36:00 +0300

    Насовал RESTRICT'ов - и гатова...

---
+++ index.sql
@@ -0,0 +1,6 @@
+CREATE INDEX date_index ON appointments USING btree("date");
+CREATE INDEX addr_clnc_index ON clinics USING hash("addresses_id");
+CREATE INDEX addr_pat_index ON patients USING hash("addresses_id");
+CREATE INDEX pat_index on _users USING hash("patients_id");
+CREATE INDEX spec_index on doctors USING hash("specialities_id");
+CREATE INDEX sev_index on symptoms USING hash("severity_id")
