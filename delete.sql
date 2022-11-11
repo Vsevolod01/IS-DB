5042b3edfc51286e5be3ba5b9c80903a10c30b6f
Vsevolod01 <seva10052001@gmail.com>
2022-11-11 03:20:47 +0300

    Насовал RESTRICT'ов - и гатова...

---
+++ delete.sql
@@ -0,0 +1,28 @@
+DELETE
+FROM appointments;
+DELETE
+FROM work;
+DELETE
+FROM status;
+DELETE
+FROM points;
+DELETE
+FROM doctors;
+DELETE
+FROM specialities;
+DELETE
+FROM rec_sym;
+DELETE
+FROM recommendations;
+DELETE
+FROM symptoms;
+DELETE
+FROM severity;
+DELETE
+FROM _users;
+DELETE
+FROM patients;
+DELETE
+FROM clinics;
+DELETE
+FROM addresses;
