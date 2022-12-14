37061b586ee7ec34f19e91b999c8503b809497dd
Vsevolod01 <seva10052001@gmail.com>
2022-12-14 13:27:41 +0300

    Composite-id classes should override equals() and hashCode()

--- src/main/java/com/example/isdb/data/PK/PointsPK.java
+++ src/main/java/com/example/isdb/data/PK/PointsPK.java
@@ -2,9 +2,11 @@ package com.example.isdb.data.PK;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
 
+@EqualsAndHashCode
 @Embeddable
 public class PointsPK implements Serializable {
     @Column(name = "symptoms_id")
--- src/main/java/com/example/isdb/data/PK/WorkPK.java
+++ src/main/java/com/example/isdb/data/PK/WorkPK.java
@@ -2,9 +2,11 @@ package com.example.isdb.data.PK;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
 
+@EqualsAndHashCode
 @Embeddable
 public class WorkPK implements Serializable {
     @Column(name = "doctors_id")
