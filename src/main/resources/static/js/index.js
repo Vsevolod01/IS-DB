8834ecaca01643621155a3f16feb10e983374a5d
Vsevolod01 <seva10052001@gmail.com>
2022-12-14 23:43:42 +0300

    Added limits for severity (id="cnt")

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -14,5 +14,5 @@ function openForm() {
         <option disabled selected>Симптомы</option>
     </select>`);
     document.getElementById("cnt").insertAdjacentHTML("beforeend", `<label for="number">Степень тяжести</label>
-    <input id="number" type="number" class="in" placeholder="От 1 до 10">`);
+    <input id="number" type="number" class="in" min="1" max="3" placeholder="От 1 до 3">`);
   }
