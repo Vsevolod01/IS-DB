ce84d9ef3f16f4def862ac0581f7a16e2f53e6d8
Vsevolod01 <seva10052001@gmail.com>
2022-12-14 23:34:29 +0300

    Added Thymeleaf, MVC and Irina's frontend

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,18 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    document.getElementById("white").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("white").style.display = "none";
+  }
+
+  function addRow() {
+    document.getElementById("sym").insertAdjacentHTML("beforeend", `<label for="symptom">Выберите симптом</label>
+    <select id="symptom" name="symptom" class="in">
+        <option disabled selected>Симптомы</option>
+    </select>`);
+    document.getElementById("cnt").insertAdjacentHTML("beforeend", `<label for="number">Степень тяжести</label>
+    <input id="number" type="number" class="in" placeholder="От 1 до 10">`);
+  }
