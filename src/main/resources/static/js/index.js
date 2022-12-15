a1bfed2bb516fa334aadb3944588bb7d32dca3d0
Vsevolod01 <seva10052001@gmail.com>
2022-12-15 19:26:16 +0300

    Severity should be taken from the table instead of given by the user

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -132,8 +132,6 @@ function addRow() {
     <select id="symptom" name="symptom" class="in">
         <option disabled selected>Симптомы</option>
     </select>`);
-    document.getElementById("cnt").insertAdjacentHTML("beforeend", `<label for="number">Степень тяжести</label>
-    <input id="number" type="number" class="in" min="1" max="3" placeholder="От 1 до 3">`);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
