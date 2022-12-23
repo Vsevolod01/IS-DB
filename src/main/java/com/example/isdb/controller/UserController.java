e8b64766c87df105ab3bd0d83b079c1c8b6a42b8
Vsevolod01 <seva10052001@gmail.com>
2022-12-23 09:07:43 +0300

    Added JRE target version and plugin for JAR building

--- src/main/java/com/example/isdb/controller/UserController.java
+++ src/main/java/com/example/isdb/controller/UserController.java
@@ -5,7 +5,6 @@ import com.example.isdb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.Console;
 import java.util.List;
 
 @RestController
