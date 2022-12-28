373b4f21cb421eec658852183bcb0e64d4feb04d
Vsevolod01 <seva10052001@gmail.com>
2022-12-28 12:36:02 +0300

    Added Lombok for AppointmentParams (although I'm still not sure if we need it)

--- src/main/java/com/example/isdb/data/AppointmentParams.java
+++ src/main/java/com/example/isdb/data/AppointmentParams.java
@@ -1,24 +1,11 @@
 package com.example.isdb.data;
 
+import lombok.Getter;
+
+@Getter
 public class AppointmentParams {
     private String district;
     private Long clinic;
     private String specialist;
     private String doctor;
-
-    public String getDistrict() {
-        return district;
-    }
-
-    public long getClinic() {
-        return clinic;
-    }
-
-    public String getSpecialist() {
-        return specialist;
-    }
-
-    public String getDoctor() {
-        return doctor;
-    }
 }
