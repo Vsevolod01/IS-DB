a0f4ea4de70a245b9333181725a58bffe9759b55
LFiosx <kurnosovaiv@mail.ru>
2022-12-30 23:45:39 +0300

    ApintnmentParam class

---
+++ src/main/java/com/example/isdb/data/AppointmentParams.java
@@ -0,0 +1,24 @@
+package com.example.isdb.data;
+
+public class AppointmentParams {
+    private String district;
+    private Long clinic;
+    private String specialist;
+    private String doctor;
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public long getClinic() {
+        return clinic;
+    }
+
+    public String getSpecialist() {
+        return specialist;
+    }
+
+    public String getDoctor() {
+        return doctor;
+    }
+}
